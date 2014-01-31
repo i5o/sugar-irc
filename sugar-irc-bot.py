21271b308217c96d6a1a6b9332cd8103c406bd02
Ignacio Rodríguez <ignacio@sugarlabs.org>
2014-01-30 23:25:00 -0200

    Add a system for log-in, we need to talk with freenode admins for get a bot cloak.

--- sugar-irc-bot.py
+++ sugar-irc-bot.py
@@ -17,6 +17,8 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import getpass
+
 from twisted.internet import reactor, protocol
 from twisted.words.protocols import irc
 
@@ -41,6 +43,9 @@ IGNORED_BOTS = ["meeting", "soakbot", "gcibot"]
 class SugarIRCBOT(irc.IRCClient):
     nickname = "sugarbot"
     realname = "Sugar Labs help bot"
+    username = "sugarbot"
+    password = getpass.getpass("irc password: ")
+
 
     def connectionMade(self):
         irc.IRCClient.connectionMade(self)
