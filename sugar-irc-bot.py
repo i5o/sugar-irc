44c60969c946d5e261d530bd00fd74693b6db31b
Ignacio Rodríguez <ignacio@sugarlabs.org>
2014-02-07 18:04:11 -0200

    Works now..

--- sugar-irc-bot.py
+++ sugar-irc-bot.py
@@ -85,9 +85,8 @@ class SugarIRCBOT(irc.IRCClient):
 
         # Restart the bot if the user is sugarbot-git
         # We need to find a elegant way.
-        if nice_user == "sugarbot-git" and  \
-            (msg.startswith("[sugar-irc] ") and "pushed" in msg and \
-                " to master" in msg):
+        if "sugarbot-git" in nice_user and "[sugar-irc] " in msg \
+            and "pushed" in msg and " to master" in msg:
             program_path = os.path.dirname(os.path.abspath(__file__))
             justnow_path = os.getcwd()
             os.chdir(program_path)
