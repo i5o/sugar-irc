6ec7a5471f1ac844cd82d8e118c5bf3044375d0c
Ignacio Rodríguez <ignacio@sugarlabs.org>
2014-02-09 02:23:35 -0200

    password, and elsalvador now has the host.

--- reloader.py
+++ reloader.py
@@ -4,7 +4,12 @@ from subprocess import call, Popen
 from flask import Flask, render_template
 app = Flask(__name__)
 
-PASSWORD = 'dellinspiron1520'
+PASSWORD = open('password.txt', 'r')
+PASSWORD = PASSWORD.read()
+
+if PASSWORD[-1:] == "\n":
+    PASSWORD = PASSWORD[:-1]
+ 
 ROOT_DIR = '.'
 GIT_DIR = os.path.join(ROOT_DIR, 'sugar-irc')
 
@@ -30,5 +35,5 @@ if __name__ == '__main__':
              cwd=ROOT_DIR)
 
     Popen(['python', 'sugar-irc-bot.py', PASSWORD], cwd=GIT_DIR)
-    app.run(port=9123)
+    app.run(host="elsalvador.treehouse.su", port=9123)
 
